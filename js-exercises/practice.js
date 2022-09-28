a1f4cd21af3865ea9873a4e38565552a87939f23
Eric Harris <ericoldie@gmail.com>
2022-09-28 09:15:07 -0400

    Start process

---
+++ js-exercises/practice.js
@@ -0,0 +1,7 @@
+var customID = 21313123
+var customFirMidLasName = "Eric Jonathon Harris, Denver, Colorado"
+var customGender = "Male"
+var customDOB = 8-18-1999
+var customDrLi = 31423413514
+var customAutoPolNum = 241343155
+
